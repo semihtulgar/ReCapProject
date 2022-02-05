1702c2bab3fef3f8f67cb3d613ff69828eb621a0
Semih TULGAR <semihtulgar@gmail.com>
2022-02-05 16:46:18 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            // İş Kodları
+            // Yetkisi var mı?
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    // Car ile ilgili veritabanında yapacağım operasyonları (CRUD) içeren interface
+    public interface ICarDal
+    {
+        // GetAll
+        List<Car> GetAll();
+
+        // Add
+        void Add(Car car);
+
+        // Update
+        void Update(Car car);
+
+        // Delete
+        void Delete(Car car);
+
+        // GetById
+        Car GetById(int id);
+
+    }
+}
